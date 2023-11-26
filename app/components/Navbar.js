643a1f7cc164dacddf8b153f35a08cfe62bd1127
blloop <42186044+blloop@users.noreply.github.com>
2023-11-25 21:22:01 -0800

    setup schemas, add first document schema

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,12 +1,17 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 export default function Navbar() {
+  const pathname = usePathname();
+  if (pathname != "/studio") {
     return (
       <nav className='border-b sticky top-0 bg-primary-500 text-primary-900 brder-primary-800 z-10'>
         <div className='h-14 max-w-7xl p-4 mx-auto flex items-center justify-between'>
           <Link href='/' className='font-semibold text-lg md:hover:underline'>My Website</Link>
           <ul className='hidden md:flex items-center justify-end space-x-4 text-lg font-medium'>
-          <li className='md:hover:underline'>
+            <li className='md:hover:underline'>
               <Link href='/blog'>Blog</Link>
             </li>
             <li className='md:hover:underline'>
@@ -20,4 +25,5 @@ export default function Navbar() {
       </nav>
     );
   }
+}
   
