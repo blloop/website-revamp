093394d48a22f7109f6a3863b8799829a335296b
blloop <42186044+blloop@users.noreply.github.com>
2024-11-11 16:05:59 -0800

    add experience schema and page content

---
+++ sanity/schema/documents/experience.js
@@ -0,0 +1,40 @@
+export default {
+  name: "experience",
+  title: "Experience",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "company",
+      title: "Company",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "date",
+      title: "Date",
+      type: "date",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "logo",
+      title: "Logo",
+      type: "image",
+      validation: (Rule) => Rule.required(),
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
--- sanity/schema/index.js
+++ sanity/schema/index.js
@@ -1,6 +1,7 @@
 import blogPost from "./documents/blogPost";
+import experience from "./documents/experience";
 import project from "./documents/project";
 
 export const schema = {
-  types: [blogPost, project],
+  types: [blogPost, experience, project],
 };
