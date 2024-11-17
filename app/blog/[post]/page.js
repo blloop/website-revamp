bf0206703d6444db654beda0a1561ba6e7ab214d
blloop <42186044+blloop@users.noreply.github.com>
2024-11-17 13:58:42 -0800

    add project and blog page layouts

--- app/blog/[post]/page.js
+++ app/blog/[post]/page.js
@@ -4,6 +4,8 @@ import { urlForImage } from "/sanity/lib/image";
 import { tryGetImageDimensions } from "@sanity/asset-utils";
 import Image from "next/image";
 import Container from "@/components/Container";
+import { ArrowLeft, Calendar } from "lucide-react";
+import Link from "next/link";
 
 const portableTextComponents = {
   types: {
@@ -15,22 +17,30 @@ export default async function Page({ params }) {
   const post = await getBlogPost(params.post);
 
   return (
-    <Container className="max-w-prose">
-      <header className="flex flex-col gap-4 items-center">
+    <Container className="!max-w-3xl text-left">
+      <Link
+        href="/blog"
+        className="w-fit inline-flex items-center text-[#859F3D] hover:underline"
+      >
+        <ArrowLeft className="h-4 w-4" />
+        Back to Blog
+      </Link>
+      <header className="flex flex-col gap-2">
         <h1 className="font-semibold text-4xl">{post[0].title}</h1>
-        <p className="font-medium text-primary-700 text-lg">
-          {post[0].description}
-        </p>
+        <p className="font-medium text-lg">{post[0].description}</p>
+        <div className="flex text-olive-300">
+          <Calendar className="w-5 h-5 mr-2" />
+          <span>{post[0].date}</span>
+        </div>
       </header>
       <Image
-        src={urlForImage(post[0].image).auto("format").size(1920, 1920).url()}
+        src={urlForImage(post[0].image).auto("format").size(1920, 1080).url()}
         width={1920}
         height={1080}
         alt={post[0].title}
-        className="h-64 w-128 object-cover rounded-2xl border border-primary-400"
+        className="w-full object-cover rounded-lg shadow-lg border-2 border-olive-300"
       />
-      <hr className="border-primary-900" />
-      <article className="prose md:prose-md prose-primary mx-auto">
+      <article className="prose prose-invert md:prose-lg mx-auto">
         <PortableText
           value={post[0].content}
           components={portableTextComponents}
@@ -66,7 +76,7 @@ function ImageComponent({ value }) {
       width={width}
       height={height}
       loading="lazy"
-      className="md:max-w-prose rounded-lg"
+      className="rounded-lg"
       style={{
         aspectRatio: width / height,
       }}
--- app/projects/[project]/page.js
+++ app/projects/[project]/page.js
@@ -4,6 +4,8 @@ import { urlForImage } from "/sanity/lib/image";
 import { tryGetImageDimensions } from "@sanity/asset-utils";
 import Image from "next/image";
 import Container from "@/components/Container";
+import { ArrowLeft, Calendar } from "lucide-react";
+import Link from "next/link";
 
 const portableTextComponents = {
   types: {
@@ -15,25 +17,37 @@ export default async function Page({ params }) {
   const project = await getProject(params.project);
 
   return (
-    <Container className="max-w-prose">
-      <header className="flex flex-col gap-4 items-center">
+    <Container className="!max-w-3xl text-left">
+      <Link
+        href="/projects"
+        className="w-fit inline-flex items-center text-[#859F3D] hover:underline"
+      >
+        <ArrowLeft className="h-4 w-4" />
+        Back to Projects
+      </Link>
+      <header className="flex flex-col gap-2">
         <h1 className="font-semibold text-4xl">{project[0].title}</h1>
-        <p className="font-medium text-primary-700 text-lg">
-          {project.description}
-        </p>
+        <p className="font-medium text-lg">{project[0].description}</p>
+        <div className="flex text-olive-300">
+          <Calendar className="w-5 h-5 mr-2" />
+          <span>{project[0].date}</span>
+        </div>
       </header>
-      <Image
-        src={urlForImage(project[0].image)
-          .auto("format")
-          .size(1920, 1920)
-          .url()}
-        width={1920}
-        height={1080}
-        alt={project[0].title}
-        className="h-64 w-128 object-cover rounded-2xl border border-primary-400"
-      />
-      <hr className="border-primary-900" />
-      <article className="prose md:prose-md prose-primary mx-auto">
+      <div className="relative w-full mb-4">
+        <div className="absolute -z-10 top-4 left-4 w-full h-full bg-olive-300 rounded-lg"></div>
+        <Image
+          src={urlForImage(project[0].image)
+            .auto("format")
+            .size(1920, 1080)
+            .url()}
+          width={1920}
+          height={1080}
+          alt={project[0].title}
+          className="w-full object-cover rounded-lg shadow-lg"
+        />
+      </div>
+      <hr className="border-olive-300" />
+      <article className="prose prose-invert md:prose-lg mx-auto">
         <PortableText
           value={project[0].content}
           components={portableTextComponents}
@@ -55,7 +69,7 @@ async function getProject(slug) {
 
   const projects = await client.fetch(query, {
     slug,
-    next: { revalidate: 84600 },
+    next: { revalidate: 3600 },
   });
   return projects;
 }
@@ -69,7 +83,7 @@ function ImageComponent({ value }) {
       width={width}
       height={height}
       loading="lazy"
-      className="md:max-w-prose rounded-lg"
+      className="rounded-lg"
       style={{
         aspectRatio: width / height,
       }}
